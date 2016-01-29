b1471ba6a502fbf0158169bf8cd069c06e2cd5a3
iosfundamentals <AIG@Dishants-Macbook-Pro.local>
2016-01-29 12:26:56 +0530

    First Commit

---
+++ SoapWebServiceDemoTests/SoapWebServiceDemoTests.m
@@ -0,0 +1,34 @@
+//
+//  SoapWebServiceDemoTests.m
+//  SoapWebServiceDemoTests
+//
+//  Created by iOSFundamentals on 25/04/14.
+//  Copyright (c) 2014 iOSFundamentals. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface SoapWebServiceDemoTests : XCTestCase
+
+@end
+
+@implementation SoapWebServiceDemoTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
